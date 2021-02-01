84a0aa8cfec0bb67091cdb9ec0bbf8e6e57616ec
Wilfried Klaas <w.klaas@gmx.de>
2021-02-01 08:45:36 +0100

    computed

--- webclient/RemoteDesk/app.js
+++ webclient/RemoteDesk/app.js
@@ -1,33 +1,29 @@
 const app = Vue.createApp({
-   data() {
-       return {
-           showBooks: true,
-           title: 'Buch',
-           author: 'Brandon',
-           age: 25,
-           x: 0,
-           y : 0
-       }
-   },
-   methods: {
-       changeTitle(title) {
-           console.log('div click')
-           this.title = title
-       },
-       toggleShowBooks() {
-           this.showBooks = !this.showBooks
-       },
-       handleEvent(e, data) {
-           console.log(e.type, e)
-           if (data) {
-               console.log(data)
-           }
-       },
-       handleMouseMove(e) {
-        this.x = e.offsetX
-        this.y = e.offsetY
+    data() {
+        return {
+            showBooks: true,
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+            ],
         }
-   }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
+        },
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => {
+                return book.isFav
+            })
+        }
+    }
 })
 
 app.mount('#app')
